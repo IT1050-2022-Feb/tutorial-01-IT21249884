cb61e3ff1784aeb75f0da980ae168d57578d6eaa
IT21249884 <99408999+IT21249884@users.noreply.github.com>
2022-02-11 04:46:32 +0000

    I completed the 1st exercise of the tutorial 01

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,24 @@
 #include <stdio.h>
 
 int main() {
+  float n1, n2;
+  float average, sum;
   
+  //Getting user inputs...
+  printf(" Enter the first mark...  "); //prompt
+  scanf("%f", &n1);   //Getting 1st mark
+
+  printf(" Enter the second mark...  ");//prompt
+  scanf("%f", &n2);   //Getting 2nd mark
+
+  //Calculations
+  sum = n1 + n2;//Calculating the sum of the two numbers
+
+  average = sum / 2.0;//Calculating the average
+
+  //Displaying the output
+  printf(" The average of the two marks is..\n %.2f", average);
+
   return 0;
 }
 
